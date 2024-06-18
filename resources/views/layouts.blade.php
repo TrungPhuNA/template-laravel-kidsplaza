f23ace17becc5fa2a85590c2bba003895f30d232
hoanganh <you@example.com>
2024-06-18 09:22:47 +0700

    optimal code

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -6,15 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Kids Plaza - Hệ thống Mẹ Bầu & Em Bé đáng tin cậy nhất Việt Nam')</title>
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <!-- Bootstrap JavaScript library -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <!-- OwlCarousel CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-    <!-- OwlCarousel JavaScript -->
+    @yield('style-layout')
+    <link href="{{ asset('css/components/navbar.css') }}" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-    @yield('style')
+    
 </head>
 
 <body>
@@ -30,16 +28,11 @@
         @endif
         @if(Route::currentRouteNamed('cart'))
             @include('components/navbar',['page'=>'cart'])
-        @else
-            @include('components/navbar',['page'=>''])
-        @endif
+        @endif 
         <main class="bg-cF pt-4 ">
             @yield('content')
         </main>
         @include('components/footer')
-        <!-- <div class="alter-web position-absolute top-0 end-0 p-5" style="z-index:99;">
-            <x-alert bg="alert-success" text="Đăng ký thành công"></x-alert>
-        </div> -->
     </div>
     <x-modal />
 </body>
--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -1,13 +1,19 @@
 @extends('layouts')
+@section('style-layout')
+    <link href="{{ asset('css/pages/home/home.css') }}" rel="stylesheet">
+@endsection
 @section('content')
-<link href="{{ asset('css/pages/home/home.css') }}" rel="stylesheet">
+@php
+    $data_static = json_decode(file_get_contents(storage_path('app/database/data-static.json')), true);
+    $banners = $data_static['banners'];
+    $diapers_hygiene = $data_static['diapers_hygiene'];
+    $milk_for_babie = $data_static['milk_for_babie'];
+    $mobile_bottom = $data_static['mobile_bottom'];
+@endphp
 <div class="position-relative">
     <div class="container px-0">
         <section class="section-wrapper">
-            @php
-            $user = auth()->user();
-            @endphp
-            @include('pages/home/include/header', ['user' => $user])
+            @include('pages/home/include/header')
         </section>
         <section class="section-wrapper">
             @include('pages/home/include/service-banner')
@@ -21,12 +27,8 @@
     </section>
     <section class="section-wrapper d-xs-none">
         <div class="banner-home-owl-carousel owl-carousel owl-theme container px-0">
-            @foreach([
-            'https://cdn-v2.kidsplaza.vn/media/mageplaza/bannerslider/banner/image/h/e/healthy_care_t5-web_m_i_banner_danh_m_c_l_n_web-1.png',
-            'https://cdn-v2.kidsplaza.vn/media/mageplaza/bannerslider/banner/image/s/k/skinny-bn_mebaudenlatang_1.jpg',
-            'https://cdn-v2.kidsplaza.vn/media/mageplaza/bannerslider/banner/image/1/2/1204x250_1.png'
-            ] as $banner)
-            <img class="rounded-4 " src="{{$banner}}" alt="img" width="100%" height="250">
+            @foreach($banners as $banner)
+                <img class="rounded-4 " src="{{$banner}}" alt="img" width="100%" height="250">
             @endforeach
         </div>
     </section>
@@ -34,16 +36,10 @@
         @include('pages/home/include/list-product')
     </section>
     <section class="section-wrapper">
-        <x-frame-product-layout-display :title="'Bỉm tã và vệ sinh'" :nav="['Tã - Bỉm Goon', 'Tã - Bỉm Merries', 'Tã - Bỉm Huggies', 'Tã - Bỉm Bobby', 'Tã - Bỉm Moony']" :banner="[
-        ['col' => 'col-7', 'src' => 'images/banner/KV_Moony_T6.png'], 
-        ['col' => 'col', 'src' => 'images/banner/KV_Huggies_xanh.png']
-    ]" :properties="[['Việt Nam', 'Nhật Bản', 'Trung Quốc', 'Italia', 'Hàn Quốc'], ['M', 'L', 'XL', 'NB', 'S', 'XXL']]" />
+        <x-frame-product-layout-display :title=" $diapers_hygiene['title'] " :nav=" $diapers_hygiene['nav'] " :banner=" $diapers_hygiene['banner'] " :properties=" $diapers_hygiene['properties'] " />
     </section>
     <section class="section-wrapper">
-        <x-frame-product-layout-display :title="'Sữa cho bé'" :nav="['Sữa Meiji','Sữa Morinaga','Sữa Nan','Sữa Aptamil','Sữa Pediasure','Sữa Similac']" :banner="[
-        ['col' => 'col', 'src' => 'images/banner/sua_cho_be_banner_1.png'], 
-        ['col' => 'col-7', 'src' => 'images/banner/sua_cho_be_banner_2.png']
-    ]" :properties="[['Việt Nam', 'Nhật Bản', 'Hàn Quốc', 'Thái Lan', 'Thủy Sĩ'], ['6M+','1Y+','0-6M','0-12M','6-12M','1-3Y']]" />
+        <x-frame-product-layout-display :title=" $milk_for_babie['title'] " :nav=" $milk_for_babie['nav'] " :banner=" $milk_for_babie['banner'] " :properties=" $milk_for_babie['properties'] " />
     </section>
     <section class="section-wrapper">
         <x-products-just-for-you bg="bg-blue-d1" carousel="" />
@@ -56,14 +52,7 @@
     </section>
     <section class="position-fixed bottom-0 w-100 d-sm-none bg-white py-3 shadow00" style="z-index:999;">
         <div class="row">
-            @foreach(
-            [
-            ['icon' => 'https://cdn.kidsplaza.io/assets/icons/kid-logo-circle.png', 'title' => 'Trang chủ'],
-            ['icon' => 'https://cdn.kidsplaza.io/assets/icons/inactive-category.png', 'title' => 'Danh mục'],
-            ['icon' => 'https://cdn.kidsplaza.io/assets/icons/inactive-gift.png', 'title' => 'Khuyến mãi'],
-            ['icon' => 'https://cdn.kidsplaza.io/assets/icons/inactive-account.png', 'title' => 'Tôi']
-            ] as $key => $item
-            )
+            @foreach( $mobile_bottom as $key => $item)
             <div class="col-3 text-center position-relative">
                 @if($key === 0)
                 <div class="" style="height:34px;">
